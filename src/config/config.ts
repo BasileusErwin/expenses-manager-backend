6ddec47584153dc1932d54a45cb18b42f7a4bc23
Ignacio Perez <ignacio.perez2123@protonmail.com>
2023-06-30 19:51:42 -0300

    Revert "Revert "Change env""
    This reverts commit 8a47f16303ffbdf78ccf9b962381340c6475d294.

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,6 +4,6 @@ export const config = {
   port: parseInt(process.env.PORT) || 3000,
   redisUrl: process.env.REDIS_URL,
   sessionSecret: process.env.SESSION_SECRET,
-  mongodbUrl: process.env.MONGODB_URL,
+  mongodbUrl: process.env.MONGO_URL,
   corsOrigins: JSON.parse(process.env.CORS_ORIGINS),
 };
