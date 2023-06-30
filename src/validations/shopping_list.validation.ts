4bb4a39104d495139e4ab4c3f10e8bb6867a37b7
Ignacio Perez <ignacio.perez2123@protonmail.com>
2023-06-30 19:51:20 -0300

    Revert "Revert "Added converte shopping item to transaction""
    This reverts commit 3948a8fc50b29dd3daf458c1e945e2a323bcb2ab.

--- src/validations/shopping_list.validation.ts
+++ src/validations/shopping_list.validation.ts
@@ -1,26 +1,7 @@
 import { body, param } from 'express-validator';
-import { CurrencyEnum } from '../enums';
-
-interface ShoppingListItemModel {
-  _id: string;
-  name: string;
-  quantity: number;
-  price: number;
-  exchangeRate: number;
-  currency: CurrencyEnum;
-  checked: boolean;
-  createdAt: Date;
-  updatedAt: Date;
-}
-
-interface ShoppingListModel {
-  _id: string;
-  userId: string;
-  name: string;
-  items: ShoppingListItemModel[];
-  createdAt: Date;
-  updatedAt: Date;
-}
+import moment from 'moment';
+import { dayHelper } from '../helpers';
+import { CurrencyEnum, MonthEnum, TransactionType } from '../enums';
 
 const createShoppingList = [
   body('shoppingList', 'Please enter a shopping list').if(body('type').not().exists()).isArray(),
@@ -32,8 +13,36 @@ const shoppingListIdInParam = [param('itemId').isUUID()];
 
 const itemIdInParam = [param('listId').isUUID()];
 
+const convertShoppingListItemToTransaction = [
+  param('itemId').isUUID(),
+  body('day', 'Please enter a day')
+    .optional()
+    .isInt({ min: 1, max: 31 })
+    .custom((value: number, { req }) => value > 0 && value <= dayHelper.getMaxDayByMonth(req.body.month))
+    .trim(),
+  body('month', 'Please enter a month').isIn(Object.values(MonthEnum)).trim(),
+  body('year', 'Please enter a year').isInt({ min: 2000, max: moment().year() }).trim(),
+  body('note', 'Please enter a note').optional().isString(),
+  body('categoryId', 'Please enter a categoryId or category')
+    .if(body('transactions').not().exists())
+    .if(body('category').not().exists())
+    .isUUID(),
+  body('category', 'Please enter a categoryId or category')
+    .if(body('transactions').not().exists())
+    .if(body('categoryId').not().exists())
+    .isObject(),
+  body('category.type', 'Please enter a category type, and make it equal to the transaction type.')
+    .if(body('category').exists())
+    .optional()
+    .isIn(Object.values(TransactionType))
+    .custom((value: TransactionType, { req }) => value === req.body.type),
+  body('category.name', 'Please enter a name').if(body('category').exists()).isString().trim(),
+  body('goalId', 'Please enter a goal id').optional().isUUID(),
+];
+
 export const shoppingListValidation = {
   createShoppingList,
   shoppingListIdInParam,
   itemIdInParam,
+  convertShoppingListItemToTransaction,
 };
